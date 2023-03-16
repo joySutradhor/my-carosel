25a69efc1ca43befc622965ea96680b6565d4240
joySutradhor <joysutradhorcmt@gmail.com>
2023-03-16 23:42:46 +0600

    my simple carosel

---
+++ my-carousel/js/slider.js
@@ -0,0 +1,56 @@
+// const images = [
+//     'images/pic-1.jpg', // 0
+//     'images/pic-2.jpg', // 1
+//     'images/pic-3.jpg',
+//     'images/pic-4.jpg',
+//     'images/pic-5.jpg',
+//     'images/pic-6.jpg',
+//     'images/pic-7.jpg',
+//     'images/pic-8.jpg',
+//     'images/pic-9.jpg', // 8
+// ]
+
+// let imgIndex = 0;
+// const imgEl = document.getElementById('slider-img');
+// setInterval( () => {
+//     if(imgIndex === images.length){
+//         imgIndex = 0;
+//     }
+//     const imgUrl = images[imgIndex];
+//     // console.log(imgIndex, imgUrl);
+//     imgEl.setAttribute('src', imgUrl);
+//     imgIndex++;
+// } , 1000)
+
+
+
+
+
+let arr = ["images/pic-1.jpg", "images/pic-2.jpg", "images/pic-3.jpg", "images/pic-4.jpg" ];
+let sliderImg = document.getElementById("slider-img");
+let index = 0; 
+let sliderFn = setInterval(()=>{
+    if(index === arr.length){
+        index = 0
+    }
+sliderImg.setAttribute("src", arr[index++])
+
+},2000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
